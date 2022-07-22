fbb68a91aeb1e336a828b5f4f0d3a08eb1249d08
Learn and anything else <lovandong342@gmail.com>
2022-07-22 11:01:33 +0700

    first commit

---
+++ assets/js/subjs.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.fa-search').on('click', function(){
+        $('.model').toggle('show');
+    })
+    $('.fa-times').on('click', function(){
+        $('.model').toggle('hide');
+    })
+    $('#close').on('click', function(){
+        $('.center-list').css('display','none');
+    })
+})
+function onclickFunction(){
+    $('.nav-main-list li a').on('click', function(){
+        $('.nav-main-list li a').removeClass('active');
+        $(this).addClass('active');
+    })
+}
+function open1(){
+   $('#list-item2').hide();
+   $('#list-item3').hide();
+   $('#list-item1').show();
+}
+function open2(){
+    $('#list-item2').show().css('display','grid');
+    $('#list-item3').hide();
+    $('#list-item1').hide();
+}
+function open3(){
+    $('#list-item2').hide();
+    $('#list-item3').show().css('display','grid');
+    $('#list-item1').hide();
+}
